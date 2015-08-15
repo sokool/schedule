3412282ec688cf9be0a817a1a030a7db318d7930
Michał Sokołowski <michal.sokolowski@smtsoftware.com>
2015-08-15 16:28:51 +0200

    interpreter pattern implementation

--- src/MintSoft/Schedule/src/MintSoft/Schedule/Expression/Set/Difference.php
+++ src/MintSoft/Schedule/src/MintSoft/Schedule/Expression/Set/Difference.php
@@ -1,20 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sokool
- * Date: 25/08/14
- * Time: 09:38
- */
 
 namespace MintSoft\Schedule\Expression\Set;
 
-use MintSoft\Schedule\Event\EventInterface;
 use MintSoft\Schedule\Expression\ExpressionInterface;
 
 class Difference implements ExpressionInterface
 {
-    public function includes(\DateTime $date, EventInterface $event)
+    public function includes(\DateTime $date)
     {
-        // TODO: Implement includes() method.
     }
 }
